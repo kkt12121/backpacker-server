16ea16ad09a5256e88b49ffcea1eb8d171f859d3
kkt12121 <kkt34343@gmail.com>
2021-06-02 13:15:43 +0900

    🔧김경태/ itemAverageCost 수정

--- controller/content/itemAverageCost.ts
+++ controller/content/itemAverageCost.ts
@@ -12,22 +12,25 @@ export default async (
     if (place) {
       // 입력한 place와 일치하는 item들을 찾는다.
       const findItem: any = await item.find({ place: place });
-
-      // 찾은 item들의 가격을 배열안에 넣는다
-      const itemCostArr: number[] = [];
-      findItem.filter((el: any) => {
-        if (el.price) {
-          return itemCostArr.push(el.price);
-        }
-      });
-      // 배열안에 있는 아이템별 비용의 합계를 구한다
-      const totalItemCost = itemCostArr.reduce((acc: number, cur: number) => {
-        acc += cur;
-        return acc;
-      }, 0);
-      // 비용의 평균을 내고 반올림한다
-      const averageItemCost = Math.floor(totalItemCost / itemCostArr.length);
-      res.status(200).json({ averageCost: averageItemCost });
+      if (findItem.length !== 0) {
+        // 찾은 item들의 가격을 배열안에 넣는다
+        const itemCostArr: number[] = [];
+        findItem.filter((el: any) => {
+          if (el.price) {
+            return itemCostArr.push(el.price);
+          }
+        });
+        // 배열안에 있는 아이템별 비용의 합계를 구한다
+        const totalItemCost = itemCostArr.reduce((acc: number, cur: number) => {
+          acc += cur;
+          return acc;
+        }, 0);
+        // 비용의 평균을 내고 반올림한다
+        const averageItemCost = Math.floor(totalItemCost / itemCostArr.length);
+        res.status(200).json({ averageCost: averageItemCost });
+      } else {
+        res.status(200).json({ averageCost: 0 });
+      }
     } else {
       return res.status(400).json({ message: "장소를 입력해 주세요 !" });
     }
